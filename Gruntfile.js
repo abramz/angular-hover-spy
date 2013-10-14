f293f3ca0d672571ef848b350b848a49c71ac640
abramz <andrew.shapro@gmail.com>
2013-10-14 12:47:17 -0700

    changed namespace format, added separate attribute for namespace, added min.map file to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,22 @@ module.exports = function (grunt) {
       ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
       ' * License: <%= pkg.license %>\n' +
       ' */\n',
-
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['**'], 
+          dest: 'build/'
+        }]
+      }
+    },
     // Task configuration.
     uglify: {
       options: {
         banner: '<%= banner %>',
-        report: 'gzip'
+        report: 'gzip',
+        sourceMap: 'build/hover-spy.min.map.js'
       },
       build: {
         src: 'src/hover-spy.js',
@@ -46,11 +56,12 @@ module.exports = function (grunt) {
   });
 
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('default', ['jshint', 'karma:unit', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'karma:unit', 'copy', 'uglify']);
   grunt.registerTask('test', ['karma:unit']);
   grunt.registerTask('test-watch', ['karma:watch']);
   grunt.registerTask('build', ['default']);
